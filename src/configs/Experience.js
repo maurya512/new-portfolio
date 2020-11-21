6bfe7ecfabefd97bd97647456570bcffb38b58d6
Maurya Patel <patelmaurya0512@gmail.com>
2020-11-21 11:52:44 -0500

    work experience

---
+++ src/configs/Experience.js
@@ -0,0 +1,25 @@
+import pp from "../assets/experience/pp.png";
+import ds from "../assets/experience/ds.gif";
+export const exp = [
+  {
+    title: "WEB DEVELOPER - FREELANCE",
+    date: "SEPT 2020 - PRESENT",
+    desc: [
+      "Working on individual projects.",
+      "Tutoring fellow students.",
+      "Building new projects to improve current skill set."
+    ],
+    img:
+      ds
+  },
+  {
+    title: "Pharmacy Technician",
+    date: "Parlin Pharmacy , Parlin -SEPT 2017 - PRESENT",
+    desc: [
+      "Getting in touch with insurance companies to help solve patient's medicinal needs.",
+      "Help patients refill their prescriptions by getting in touch with the doctor's offices.",
+      "Refilling medications, constituting Antibiotics and more."
+    ],
+    img: pp
+  }
+];
